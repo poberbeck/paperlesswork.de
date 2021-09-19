af3e3cd63888c98be38d5611bf9fccfc17390375
POberbeck <pascal@oberbecks.de>
2021-09-19 23:06:30 +0200

    Add mobile Menue

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -1,29 +1,66 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { Transition } from '@headlessui/react';
+
+const menueItems = [
+    { name: 'Home', link: '/' },
+    { name: 'Meine Arbeit', link: '/work' },
+    { name: 'Kontakt', link: '/contact' },
+];
 
 const NavigationBar: React.FC = () => {
+    const router = useRouter();
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
     return (
-        <div className='container mx-auto'>
-            <div className='flex justify-between items-center p-6'>
-                <img className='h-16 w-16' src='/images/plwo_logo.svg' alt='Paperless Work Logo' />
-                <div className='flex space-x-6'>
-                    <Link href='/'>
-                        <a className='text-indigo-700'>
-                            <p>Home</p>
-                        </a>
-                    </Link>
-                    <Link href='/'>
-                        <a>
-                            <p>Meine Arbeit</p>
-                        </a>
-                    </Link>
-                    <Link href='/'>
-                        <a>
-                            <p>Kontakt</p>
-                        </a>
-                    </Link>
+        <div className='relative container mx-auto'>
+            <Transition
+                as={Fragment}
+                show={mobileMenuOpen}
+                enter='transform duration-200 transition ease-in-out'
+                enterFrom='opacity-0'
+                enterTo='opacity-100'
+                leave='transform duration-200 transition ease-in-out'
+                leaveFrom='opacity-100'
+                leaveTo='opacity-0'>
+                <div className='absolute z-50 top-1 right-0 left-0 bg-white rounded-2xl shadow-md p-4'>
+                    <div className='flex justify-between'>
+                        <p className='font-bold text-lg'>Menü</p>
+                        <button onClick={() => setMobileMenuOpen(false)}>
+                            <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                                <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M6 18L18 6M6 6l12 12' />
+                            </svg>
+                        </button>
+                    </div>
+                    <div className='mt-4 flex flex-col space-y-4'>
+                        {menueItems.map((item, key) => (
+                            <Link key={key + item.link} href={item.link}>
+                                <a className={router.pathname === item.link ? 'text-indigo-700 underline' : ''}>
+                                    <p>{item.name}</p>
+                                </a>
+                            </Link>
+                        ))}
+                    </div>
                 </div>
+            </Transition>
+            <div className='relative flex justify-between items-center p-6'>
+                <img className='h-16 w-16' src='/images/plwo_logo.svg' alt='Paperless Work Logo' />
+                <div className='hidden md:flex space-x-6'>
+                    {menueItems.map((item, key) => (
+                        <Link key={key + item.link} href={item.link}>
+                            <a className={router.pathname === item.link ? 'text-indigo-700 underline' : ''}>
+                                <p>{item.name}</p>
+                            </a>
+                        </Link>
+                    ))}
+                </div>
+                <button onClick={() => setMobileMenuOpen(true)} className='block md:hidden'>
+                    <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                        <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M4 6h16M4 12h16M4 18h16' />
+                    </svg>
+                </button>
             </div>
         </div>
     );
