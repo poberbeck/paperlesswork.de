324a484e2d00f87ce82f5485bd940f07d7660bf4
POberbeck <pascal@oberbecks.de>
2021-09-20 00:00:07 +0200

    Fix spelling

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const HomePage: React.FC = () => {
                     <div className='max-w-2xl flex flex-col items-center space-y-8'>
                         <h1 className='font-bold text-4xl md:text-8xl text-center'>Ideen lebendig werden lassen</h1>
                         <p className='text-center text-gray-800'>
-                            Egal ob im Web, Mobil oder im Print, ein professionelles auftreten ist wichtig für das gewinnen von neuen Kunden. Ich helfe Ihnen diese Auftritt zu entwickeln, zu
+                            Egal ob im Web, Mobil oder im Print, ein professionelles Auftreten ist wichtig für das Gewinnen von neuen Kunden. Ich helfe Ihnen diese Auftritt zu entwickeln, zu
                             optimieren und umzusetzen.
                         </p>
 
