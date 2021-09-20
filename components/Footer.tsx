0b81a2b80c02699d74fb78c86511242de8ceff06
POberbeck <pascal@oberbecks.de>
2021-09-20 13:28:49 +0200

    Fix Footer spacing

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,43 +3,45 @@ import Link from 'next/link';
 
 const Footer: React.FC = () => {
     return (
-        <footer className='bg-gray-50 text-gray-400 p-5 flex flex-col space-y-4'>
-            <div className='bg-gray-200 rounded-full w-full h-px'></div>
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
-                <div>
-                    <p className='text-black'>Seiten</p>
-                    <Link href='/'>
-                        <a>
-                            <p>Home</p>
-                        </a>
-                    </Link>
-                    <Link href='/'>
-                        <a>
-                            <p>Meine Arbeit</p>
-                        </a>
-                    </Link>
-                    <Link href='/contact'>
-                        <a>
-                            <p>Kontakt</p>
-                        </a>
-                    </Link>
+        <div className='bg-gray-50 text-gray-400'>
+            <footer className='container mx-auto p-5 flex flex-col space-y-4'>
+                <div className='bg-gray-200 rounded-full w-full h-px'></div>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+                    <div>
+                        <p className='text-black'>Seiten</p>
+                        <Link href='/'>
+                            <a>
+                                <p>Home</p>
+                            </a>
+                        </Link>
+                        <Link href='/'>
+                            <a>
+                                <p>Meine Arbeit</p>
+                            </a>
+                        </Link>
+                        <Link href='/contact'>
+                            <a>
+                                <p>Kontakt</p>
+                            </a>
+                        </Link>
+                    </div>
+                    <div>
+                        <p className='text-black'>Legal</p>
+                        <Link href='/legal'>
+                            <a>
+                                <p>Impressum</p>
+                            </a>
+                        </Link>
+                        <Link href='/privacy'>
+                            <a>
+                                <p>Datenschutz</p>
+                            </a>
+                        </Link>
+                    </div>
                 </div>
-                <div>
-                    <p className='text-black'>Legal</p>
-                    <Link href='/legal'>
-                        <a>
-                            <p>Impressum</p>
-                        </a>
-                    </Link>
-                    <Link href='/privacy'>
-                        <a>
-                            <p>Datenschutz</p>
-                        </a>
-                    </Link>
-                </div>
-            </div>
-            <small>Copyright © {new Date().getFullYear()} Paperless Work | Pascal Oberbeck</small>
-        </footer>
+                <small>Copyright © {new Date().getFullYear()} Paperless Work | Pascal Oberbeck</small>
+            </footer>{' '}
+        </div>
     );
 };
 
