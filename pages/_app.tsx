b16a94716376e1cb096cb4124a77ab0983935220
POberbeck <pascal@oberbecks.de>
2021-09-20 12:35:50 +0200

    Add new Logo and Favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import type { AppProps } from 'next/app';
 import 'tailwindcss/tailwind.css';
 import { DefaultSeo } from 'next-seo';
 import SEO from '../next-seo.config';
+import Favicons from '../components/Favicons';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
+            <Favicons />
             <DefaultSeo {...SEO} />
             <Component {...pageProps} />
         </>
