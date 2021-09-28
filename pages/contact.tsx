b10fba2bebabf2a50f9763fb516e6ff69efadd0d
Pascal Oberbeck <35005267+poberbeck@users.noreply.github.com>
2021-09-28 17:06:30 +0200

    Abstract Component Title & SubTitle

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -3,6 +3,8 @@ import ContactForm from '../components/ContactForm';
 import Footer from '../components/Footer';
 import NavigationBar from '../components/NavigationBar';
 import { NextSeo } from 'next-seo';
+import SubTitle from '../components/SubTitle';
+import Title from '../components/Title';
 
 const contact = () => {
     return (
@@ -11,11 +13,11 @@ const contact = () => {
             <div className='min-h-screen flex flex-col bg-gray-50'>
                 <NavigationBar />
                 <div className='container mx-auto px-4 flex-1 flex flex-col items-center space-y-4 mb-8'>
-                    <h1 className='font-bold text-4xl'>Kontakt aufnehmen</h1>
-                    <p className='text-gray-800 text-center leading-loose'>
+                    <Title>Kontakt aufnehmen</Title>
+                    <SubTitle>
                         Kontaktieren Sie mich unkompliziert und unverbindlich via Email unter{' '}
                         <span className='p-2 bg-indigo-700 rounded-lg text-white font-bold m-2'>pascal.oberbeck(at)paperlesswork.de</span> oder via Kontaktformular.
-                    </p>
+                    </SubTitle>
                     <ContactForm />
                 </div>
             </div>
