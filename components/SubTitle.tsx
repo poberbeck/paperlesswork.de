009ea378d73219683e68ced29c18d91b81f2e0dd
Pascal Oberbeck <35005267+poberbeck@users.noreply.github.com>
2021-09-28 17:05:03 +0200

    Abstract Component

---
+++ components/SubTitle.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SubTitle: React.FC = ({ children }) => {
+    return <p className='text-gray-800 text-center leading-loose'>{children}</p>;
+};
+
+export default SubTitle;
---
+++ components/Title.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Title: React.FC = ({ children }) => {
+    return <h2 className='font-bold text-4xl'>{children}</h2>;
+};
+
+export default Title;
