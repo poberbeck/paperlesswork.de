af1926d397ed9c3daa83cf39624e076e763507bf
Pascal Oberbeck <35005267+poberbeck@users.noreply.github.com>
2021-09-28 21:27:04 +0200

    Move Content of Page to CMS

--- pages/legal.tsx
+++ pages/legal.tsx
@@ -2,58 +2,27 @@ import React from 'react';
 import Footer from '../components/Footer';
 import NavigationBar from '../components/NavigationBar';
 import { NextSeo } from 'next-seo';
+import { GetStaticProps } from 'next';
+import axios, { AxiosResponse } from 'axios';
+import { CockpitSimplePage } from '../types';
+import { COCKPIT_API_KEY, COCKPIT_URL } from '../constants';
 
-const LegalPage = () => {
+const LegalPage: React.FC<CockpitSimplePage> = ({ seoTitle, content }) => {
     return (
         <div>
-            <NextSeo title='Impressum' />
+            <NextSeo title={seoTitle} />
             <NavigationBar />
-            <div className='container mx-auto prose p-4'>
-                <h1>Impressum</h1>
-                <h2>Angaben gemäß § 5 TMG:</h2>
-                <address>
-                    <p>
-                        Pascal Oberbeck <br></br>
-                        Paperless Work | Pascal Oberbeck<br></br>
-                        An der Zisterne 12e<br></br>
-                        55291 Saulheim
-                    </p>
-                </address>
-
-                <h2>Kontakt:</h2>
-                <p>E-Mail: kontakt(at)paperlesswork.de</p>
-                <h2>Streitschlichtung</h2>
-                <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
-                <h2>Haftung für Inhalte</h2>
-                <p>
-                    Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als
-                    Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit
-                    hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist
-                    jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend
-                    entfernen.
-                </p>
-                <h2>Haftung für Links</h2>
-                <p>
-                    Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen.
-                    Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf
-                    mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch
-                    ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
-                </p>
-                <h2>Urheberrecht</h2>
-                <p>
-                    Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der
-                    Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den
-                    privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere
-                    werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei
-                    Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
-                </p>
-                <p>
-                    Quelle: <a href='https://www.e- recht24.de'>https://www.e-recht24.de</a>
-                </p>
-            </div>
+            <div className='container mx-auto prose p-4' dangerouslySetInnerHTML={{ __html: content }}></div>
             <Footer />
         </div>
     );
 };
 
 export default LegalPage;
+
+export const getStaticProps: GetStaticProps = async () => {
+    const cockpitResult: AxiosResponse<CockpitSimplePage> = await axios.get(COCKPIT_URL + '/api/singletons/get/legalPage?token=' + COCKPIT_API_KEY);
+    return {
+        props: { ...cockpitResult.data },
+    };
+};
