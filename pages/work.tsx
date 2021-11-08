42a65e17d4bcde38acb06540bd9976ec14de2915
POberbeck <pascal@oberbecks.de>
2021-11-08 21:29:13 +0100

    Move Content to CMS

--- pages/work.tsx
+++ pages/work.tsx
@@ -4,76 +4,57 @@ import NavigationBar from '../components/NavigationBar';
 import LinkButton from '../components/LinkButton';
 import { GetStaticProps } from 'next';
 import axios, { AxiosResponse } from 'axios';
-import { COCKPIT_API_KEY, COCKPIT_URL } from '../constants';
-import { CockpitSkillsCollection } from '../types';
+import { COCKPIT_API_KEY, COCKPIT_ASSETS_URL, COCKPIT_URL } from '../constants';
+import { CockpitFreelancingPage, CockpitQualitiesCollection, CockpitSkillsCollection } from '../types';
 import SkillCard from '../components/SkillCard';
 import { NextSeo } from 'next-seo';
-import Image from 'next/image';
+import ReactMarkdown from 'react-markdown';
+import QualityCard from '../components/QualityCard';
 
 interface Props {
     cockpitSkillsEntriesResult: CockpitSkillsCollection;
+    cockpitQualitiesEntriesResult: CockpitQualitiesCollection;
+    cockpitFreelancingPageResult: CockpitFreelancingPage;
 }
 
 const WorkPage: React.FC<Props> = (props) => {
-    const { cockpitSkillsEntriesResult } = props;
+    const { cockpitSkillsEntriesResult, cockpitFreelancingPageResult, cockpitQualitiesEntriesResult } = props;
     return (
         <div className='bg-gray-50'>
             <NavigationBar />
-            <NextSeo title={'Freelancing'} />
+            <NextSeo title={cockpitFreelancingPageResult.seoTitle} />
 
             <div className='min-h-screen container mx-auto px-4 md:px-12'>
                 <section className='flex-1 grid md:grid-cols-2 gap-10'>
                     <div className='flex space-y-8 flex-col justify-center'>
                         <h1 className='font-bold text-5xl md:text-7xl'>
-                            Nicht <span className='text-indigo-700'>Träumen</span>
-                            <br />
-                            Sondern <span className='text-indigo-700'>Machen</span>
+                            <ReactMarkdown components={{ strong: (strong) => <span className='text-indigo-700'>{strong.children}</span> }}>{cockpitFreelancingPageResult.leading}</ReactMarkdown>
                         </h1>
-                        <p className='text-gray-800 text-xl'>
-                            Als digital Freelancer bin ich Ihr Ansprechpartner für die Umsetzung Ihrer digitalen Ideen. Sie wollten schon immer eine eigene Webseite, Visitenkarte oder App? Dann zögern
-                            Sie nicht.
-                        </p>
+                        <p className='text-gray-800 text-xl'>{cockpitFreelancingPageResult.leadingSub}</p>
                         <div>
-                            <LinkButton type='primary' href='/'>
-                                Jetzt Angebot anfordern
+                            <LinkButton type='primary' href={cockpitFreelancingPageResult.ctaLink}>
+                                {cockpitFreelancingPageResult.ctaLabel}
                             </LinkButton>
                         </div>
                     </div>
                     <div className='p-4 flex justify-center items-center'>
-                        <img src='/images/dreamer.svg' alt='Dreamer Grafik' />
+                        <img src={COCKPIT_ASSETS_URL + cockpitFreelancingPageResult.heroImage.path} alt='Dreamer Grafik' />
                     </div>
                 </section>
-                <section className='py-24 space-y-8'>
-                    <h2 className='font-bold text-4xl md:text-6xl text-center'>Meine Skills</h2>
+                <section className='py-32 md:py-24 space-y-8'>
+                    <h2 className='font-bold text-4xl md:text-6xl text-center'>{cockpitFreelancingPageResult.sectionOneHeading}</h2>
                     <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
                         {cockpitSkillsEntriesResult.entries.map((item) => (
                             <SkillCard key={item._id} skillDescription={item.skillDescription} skillIcon={item.skillIcon} skillName={item.skillName} />
                         ))}
                     </div>
                 </section>
-                <section className='py-24 space-y-8 max-w-5xl mx-auto'>
-                    <h2 className='font-bold text-4xl md:text-6xl text-center'>Meine Qulitäten</h2>
+                <section className='py-32 md:py-24 space-y-8 max-w-5xl mx-auto'>
+                    <h2 className='font-bold text-4xl md:text-6xl text-center'>{cockpitFreelancingPageResult.sectionTwoHeading}</h2>
                     <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
-                        <div className='bg-gradient-to-br from-indigo-700 to-pink-700 shadow-md rounded-xl p-5 space-y-5'>
-                            <h3 className='text-white font-bold text-xl'>Schnell als Standart</h3>
-                            <p className='text-white'>
-                                Gerade für Webseiten ist es wichtig auf Performance zu achten. Eine schnelle Webpräsenz spiegelt sich in zufriedenen Kunden, schnelleren Transaktionen und einer bessern
-                                Suchmaschienenplazierungen wieder. Bei der Entwiklung Ihres Projektes liegt ein großer Fokus auf der Performance.
-                            </p>
-                            <div className='flex justify-center items-end'>
-                                <img className='h-64' alt='Barrierefreiheit Icon' src='/images/website_score.png' />
-                            </div>
-                        </div>
-                        <div className='bg-white shadow-md rounded-xl p-5 space-y-5'>
-                            <h3 className='text-indigo-700 font-bold text-xl'>Zugänglich für alle</h3>
-                            <p className=''>
-                                Jeder sollte in der lage sein, Ihre Idee zu verstehen. Dazu zählen auch Menschen mit einschränkungen. Viele digitalen Produkte achte nicht darauf, ob sie für jeden
-                                zugänglich sind oder nicht. Ich helfe Ihnen dabei Ihr Pojekt zu gestallten, dass es für jeden zugänglich ist.
-                            </p>
-                            <div className='flex justify-center items-center'>
-                                <img className='h-64' alt='Barrierefreiheit Icon' src='/images/accessibility_icon.png' />
-                            </div>
-                        </div>
+                        {cockpitQualitiesEntriesResult.entries.map((item, key) => (
+                            <QualityCard primay={key === 0} key={item._id} description={item.description} image={COCKPIT_ASSETS_URL + item.image.path} title={item.title} />
+                        ))}
                     </div>
                 </section>
             </div>
@@ -85,11 +66,15 @@ const WorkPage: React.FC<Props> = (props) => {
 export default WorkPage;
 
 export const getStaticProps: GetStaticProps = async () => {
+    const cockpitFreelancingPageResult: AxiosResponse<CockpitFreelancingPage> = await axios.get(COCKPIT_URL + '/api/singletons/get/freelancingPage?token=' + COCKPIT_API_KEY);
     const cockpitSkillsEntriesResult: AxiosResponse<CockpitSkillsCollection> = await axios.get(COCKPIT_URL + '/api/collections/get/skills?token=' + COCKPIT_API_KEY);
+    const cockpitQualitiesEntriesResult: AxiosResponse<CockpitQualitiesCollection> = await axios.get(COCKPIT_URL + '/api/collections/get/qualities?token=' + COCKPIT_API_KEY);
 
     return {
         props: {
+            cockpitFreelancingPageResult: cockpitFreelancingPageResult.data,
             cockpitSkillsEntriesResult: cockpitSkillsEntriesResult.data,
+            cockpitQualitiesEntriesResult: cockpitQualitiesEntriesResult.data,
         },
     };
 };
