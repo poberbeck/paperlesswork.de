e1da4e1329a1787eec63e84bc7966ee647555793
POberbeck <pascal@oberbecks.de>
2021-11-22 09:30:56 +0100

    Fix responsiv sizing

--- pages/success.tsx
+++ pages/success.tsx
@@ -11,11 +11,11 @@ const Success: NextPage = () => {
             <NextSeo title={'Danke sehr'} />
             <main className="min-h-screen bg-gray-50 flex flex-col">
                 <NavigationBar />
-                <div className="flex-1 flex justify-center items-center flex-col container mx-auto space-y-4">
-                    <h1 className="font-black text-7xl">
+                <div className="flex-1 flex justify-center items-center flex-col container mx-auto space-y-6">
+                    <h1 className="font-black text-4xl md:text-7xl text-center">
                         Danke für Ihre Einsendung.
                     </h1>
-                    <p className="text-gray-800 text-xl">
+                    <p className="text-gray-800 text-xl text-center">
                         Wir melden uns so bald wie möglich bei Ihnen.
                     </p>
                     <LinkButton type="primary" href="/">
