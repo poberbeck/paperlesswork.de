e541e0d7985d724f7d745812efe4252f2cab167e
POberbeck <pascal@oberbecks.de>
2021-12-02 13:01:50 +0100

    Run prettier

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    // Be sure to pass `true` as the second argument to `url.parse`.
-    // This tells it to parse the query portion of the URL.
-    const parsedUrl = parse(req.url, true)
-    const { pathname, query } = parsedUrl
+    createServer((req, res) => {
+        // Be sure to pass `true` as the second argument to `url.parse`.
+        // This tells it to parse the query portion of the URL.
+        const parsedUrl = parse(req.url, true);
+        const { pathname, query } = parsedUrl;
 
-    handle(req, res, parsedUrl)
-  }).listen(3000, (err) => {
-    if (err) throw err
-    console.log("> Running with custom Server!");
-    console.log('> Ready on http://localhost:3000')
-  })
-})
+        handle(req, res, parsedUrl);
+    }).listen(3000, (err) => {
+        if (err) throw err;
+        console.log('> Running with custom Server!');
+        console.log('> Ready on http://localhost:3000');
+    });
+});
