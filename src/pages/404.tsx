f268bb2f80e4d511f6d310c006c20367589a4a18
POberbeck <pascal@oberbecks.de>
2022-04-07 16:42:12 +0200

    Chnage UI of 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -7,24 +7,22 @@ import LinkButton from '@components/LinkButton';
 
 const NotFound: NextPage = () => {
     return (
-        <div>
+        <div className="flex min-h-screen flex-col bg-gray-50">
             <NextSeo title={'Nicht gefunden'} />
-            <main className="flex min-h-screen flex-col bg-gray-50">
-                <NavigationBar />
-                <div className="container mx-auto flex flex-1 flex-col items-center justify-center space-y-6">
-                    <h1 className="text-center text-4xl font-black md:text-7xl">
-                        Fehler <span className="text-indigo-700">404</span>.{' '}
-                        <span role="img" aria-label="Trauriger Smily">
-                            😢
-                        </span>
-                    </h1>
-                    <p className="text-center text-xl text-gray-800">
-                        Die Seite konnte nicht gefunden werden.
-                    </p>
-                    <LinkButton type="primary" href="/">
-                        Zurück zur Startseite
-                    </LinkButton>
-                </div>
+            <NavigationBar />
+            <main className="container mx-auto flex flex-1 flex-col items-center justify-center space-y-6">
+                <p className="text-center text-4xl font-black text-gray-300 md:text-7xl">
+                    Fehler 404
+                </p>
+                <h1 className="text-center text-4xl font-black md:text-6xl">
+                    Die Seite konnte nicht gefunden werden.
+                </h1>
+                <p className="text-center text-xl text-gray-800">
+                    Leider konnten wir Ihre gewünschte Seite nicht finden.
+                </p>
+                <LinkButton type="primary" href="/">
+                    Zurück zur Startseite
+                </LinkButton>
             </main>
             <Footer />
         </div>
