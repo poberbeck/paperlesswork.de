56e06647e31569ba380d2c89d3d7cd056be4a7d1
POberbeck <pascal@oberbecks.de>
2022-06-14 20:44:30 +0200

    Abstract Component

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -12,6 +12,7 @@ import { NextSeo } from 'next-seo';
 import Image from 'next/image';
 import { format } from 'date-fns';
 import Link from 'next/link';
+import BlogPostListItem from '@components/BlogPostListItem';
 
 interface Props {
     posts: PostEntity[];
@@ -24,46 +25,10 @@ const BlogHomePage: NextPage<Props> = (props) => {
         <>
             <NextSeo title="Blog" />
             <NavigationBar />
-            <main className="z-0 mx-auto min-h-screen max-w-2xl">
+            <main className="mx-auto min-h-screen max-w-3xl">
                 <ul>
                     {posts.map((post) => (
-                        <li key={post.id}>
-                            <Link href={'/blog/' + post.attributes?.slug}>
-                                <a className="z-0 m-4 flex flex-col gap-5 rounded-lg p-2 transition hover:bg-gray-200 hover:shadow-md md:flex-row">
-                                    <Image
-                                        className="overflow-hidden rounded-lg shadow-md"
-                                        objectFit="cover"
-                                        src={
-                                            process.env.NEXT_PUBLIC_ASSETS_URL +
-                                            post.attributes?.hero?.data
-                                                ?.attributes?.url!
-                                        }
-                                        height={300}
-                                        width={500}
-                                        alt={'Blog Post Image'}
-                                        blurDataURL={
-                                            post.attributes?.hero?.data
-                                                ?.attributes?.placeholder!
-                                        }
-                                        placeholder="blur"
-                                    />
-
-                                    <div className="flex w-full flex-col gap-2">
-                                        <h2 className="text-2xl font-black">
-                                            {post.attributes?.title}
-                                        </h2>
-                                        <p className="text-sm text-gray-500">
-                                            {format(
-                                                new Date(
-                                                    post.attributes?.publishedAt
-                                                ),
-                                                'dd.mm.yyyy'
-                                            )}
-                                        </p>
-                                    </div>
-                                </a>
-                            </Link>
-                        </li>
+                        <BlogPostListItem key={post.id} post={post} />
                     ))}
                 </ul>
             </main>
