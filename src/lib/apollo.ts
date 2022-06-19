86aa3a33981e299a53b7eca667919fefacdb8680
POberbeck <pascal@oberbecks.de>
2022-06-19 21:53:05 +0200

    Fix cache

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -3,6 +3,12 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 const client = new ApolloClient({
     uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
     cache: new InMemoryCache(),
+    defaultOptions: {
+        query: {
+            fetchPolicy:
+                typeof window === 'undefined' ? 'no-cache' : 'cache-first',
+        },
+    },
 });
 
 export default client;
